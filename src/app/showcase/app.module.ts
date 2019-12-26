5bbee36e71fbf07e35d1c19175e7d8cd323bafba
ahmed ataallah <ahmed45316@hotmail.com>
2019-12-26 14:30:33 +0200

    init

---
+++ src/app/showcase/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule }    from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AutoCompleteModule } from '../components/autocomplete/autocomplete';
+
+import { CarService } from './service/carservice';
+import { CountryService } from './service/countryservice';
+import { EventService } from './service/eventservice';
+import { NodeService } from './service/nodeservice';
+import { IconService } from './service/iconservice';
+import { FullCalendarComponent } from './Screens/full-calendar/full-calendar.component';
+import { FullCalendarModule } from '../components/public_api';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FullCalendarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AutoCompleteModule,
+    FullCalendarModule
+  ],
+  providers: [
+      { provide: LocationStrategy, useClass: HashLocationStrategy },
+      CarService,CountryService,EventService,NodeService,IconService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
